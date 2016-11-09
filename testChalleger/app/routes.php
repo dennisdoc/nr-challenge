c50b19d92144fd1e33f4cbd0c1fd1573d0d177d5
dennisdoc <dennisdoc@hotmail.com>
2016-11-09 21:49:30 -0200

    conversao para json funcionando

--- testChalleger/app/routes.php
+++ testChalleger/app/routes.php
@@ -18,6 +18,34 @@ Route::get('/', function()
 
 Route::get('/test/find', function()
 {
-	return '123';
+	header('Content-type: application/json; charset=UTF-8');
+	//http://www.cnpq.br/web/guest/licitacoes?p_p_id=licitacoescnpqportlet_WAR_licitacoescnpqportlet_INSTANCE_BHfsvMBDwU0V&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2&pagina=2&delta=10&registros=1298
+	$html=file_get_contents("http://www.cnpq.br/web/guest/licitacoes?p_p_id=licitacoescnpqportlet_WAR_licitacoescnpqportlet_INSTANCE_BHfsvMBDwU0V&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2&pagina=2&delta=10&registros=1298");
+	$html= mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
+	$html = tidy_repair_string($html);
+
+	$doc = new DomDocument();
+	$doc->loadHtml($html);
+	$xpath = new DomXPath($doc);
+	// Now query the document:
+	$array=array();
+	$location='//table[@class="table table-bordered table-hover table-striped"]/tbody/tr/td/div';
+	foreach ($xpath->query($location.'/h4') as $node) {
+	  $object=array();
+	  $object['licitacao']=html_entity_decode($node->nodeValue);
+	  array_push($array, $object);
+	}
+	$i=0;
+	foreach ($xpath->query($location.'/div/div/p') as $node) {
+	  $array[$i]['objeto']=html_entity_decode($node->nodeValue);
+	  $i++;
+	}
+	$i=0;
+	foreach ($xpath->query($location.'/div/div/span') as $node) {
+	  $array[$i]['publicacoes']=html_entity_decode($node->nodeValue);
+	  $i++;
+	}
+	return json_encode($array);
+	
 });
 
